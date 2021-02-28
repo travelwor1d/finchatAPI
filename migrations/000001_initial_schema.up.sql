d14ca8b073c7d27d888e4c4a2decd79a16da809f
travelwor1d <travel@git.com>
2021-02-28 15:48:01 +0100

    mysql schema - typo fix

--- migrations/000001_initial_schema.up.sql
+++ migrations/000001_initial_schema.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
   last_name varchar(40) NOT NULL,
   phone varchar(40),
   email varchar(50) NOT NULL,
-  user_type varchar() NOT NULL CHECK (user_type in ('GOAT', 'USER')),
+  user_type varchar(4) NOT NULL CHECK (user_type in ('GOAT', 'USER')),
   -- profile_avatar is a filepath.
   profile_avatar varchar(255),
   last_seen timestamp NOT NULL DEFAULT now()
