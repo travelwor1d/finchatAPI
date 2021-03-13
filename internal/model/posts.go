097336183c5d9dbd3e1b7ef87821cf27a7bfa3be
travelwor1d <travel@git.com>
2021-03-13 11:30:17 +0100

    json tags on models

--- internal/model/posts.go
+++ internal/model/posts.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Post struct {
-	ID          int        `db:"id"`
-	Title       string     `db:"title"`
-	Content     string     `db:"content"`
-	PostedBy    int        `db:"posted_by"`
-	PublishedAt *time.Time `db:"published_at"`
-	CreatedAt   time.Time  `db:"created_at"`
-	UpdatedAt   time.Time  `db:"updated_at"`
+	ID          int        `db:"id" json:"id"`
+	Title       string     `db:"title" json:"title"`
+	Content     string     `db:"content" json:"content"`
+	PostedBy    int        `db:"posted_by" json:"postedBy"`
+	PublishedAt *time.Time `db:"published_at" json:"publishedAt"`
+	CreatedAt   time.Time  `db:"created_at" json:"createdAt"`
+	UpdatedAt   time.Time  `db:"updated_at" json:"updatedAt"`
 }
