e700dd82c48aa2b9cc40eacf054223add7c76755
travelwor1d <travel@git.com>
2021-03-31 21:43:20 +0200

    schema improvements

--- migrations/000001_initial_schema.up.sql
+++ migrations/000001_initial_schema.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-  id int AUTO_INCREMENT PRIMARY KEY,
+  id int unsigned AUTO_INCREMENT PRIMARY KEY,
   -- Stripe customer id
   stripe_id varchar(50),
   first_name varchar(40) NOT NULL,
@@ -17,50 +17,60 @@ CREATE TABLE users (
 );
 
 CREATE TABLE credentials (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  user_id int NOT NULL UNIQUE REFERENCES users (id),
+  id int unsigned AUTO_INCREMENT PRIMARY KEY,
+  user_id int unsigned NOT NULL UNIQUE,
   hash varchar(255) NOT NULL,
   created_at timestamp NOT NULL DEFAULT now(),
-  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now()
+  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now(),
+
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE goat_invite_codes (
   -- Generated on row creation.
   invite_code char(6) PRIMARY KEY,
   -- User that requested invite code creation (admin or goat inviter).
-  created_by int NOT NULL REFERENCES users (id),
+  created_by int unsigned NOT NULL,
   status varchar(7) NOT NULL DEFAULT 'ACTIVE' CHECK (status IN ('ACTIVE', 'USED', 'EXPIRED')),
   -- The goat that registered using this code.
-  used_by int UNIQUE REFERENCES users (id),
+  used_by int unsigned UNIQUE,
   created_at timestamp NOT NULL DEFAULT now(),
-  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now()
+  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now(),
+
+  FOREIGN KEY (created_by) REFERENCES users (id),
+  FOREIGN KEY (used_by) REFERENCES users (id)
 );
 
 CREATE TABLE posts (
-  id int AUTO_INCREMENT PRIMARY KEY,
+  id int unsigned AUTO_INCREMENT PRIMARY KEY,
   title varchar(255) NOT NULL,
   content text NOT NULL,
   -- Comma separated list of image urls.
   image_urls varchar(6553) NOT NULL DEFAULT '',
   tickers varchar(6553) NOT NULL DEFAULT '',
-  posted_by int NOT NULL REFERENCES users (id),
+  posted_by int unsigned NOT NULL,
   published_at timestamp,
   created_at timestamp NOT NULL DEFAULT now(),
-  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now()
+  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now(),
+
+  FOREIGN KEY (posted_by) REFERENCES users (id)
 );
 
 CREATE TABLE comments (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  post_id int NOT NULL REFERENCES posts (id),
+  id int unsigned AUTO_INCREMENT PRIMARY KEY,
+  post_id int unsigned NOT NULL,
   content text NOT NULL,
-  posted_by int NOT NULL REFERENCES users (id),
+  posted_by int unsigned NOT NULL,
   published_at timestamp,
   created_at timestamp NOT NULL DEFAULT now(),
-  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now()
+  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now(),
+
+  FOREIGN KEY (post_id) REFERENCES posts (id),
+  FOREIGN KEY (posted_by) REFERENCES users (id)
 );
 
 CREATE TABLE threads (
-  id int AUTO_INCREMENT PRIMARY KEY,
+  id int unsigned AUTO_INCREMENT PRIMARY KEY,
   title varchar(255) DEFAULT '',
   thread_type varchar(6) NOT NULL CHECK (thread_type in ('SINGLE', 'GROUP')),
   created_at timestamp NOT NULL DEFAULT now(),
@@ -68,19 +78,25 @@ CREATE TABLE threads (
 );
 
 CREATE TABLE thread_participants (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  user_id int NOT NULL REFERENCES users (id),
-  thread_id int NOT NULL REFERENCES threads (id),
+  id int unsigned AUTO_INCREMENT PRIMARY KEY,
+  user_id int unsigned NOT NULL REFERENCES users (id),
+  thread_id int unsigned NOT NULL REFERENCES threads (id),
   created_at timestamp NOT NULL DEFAULT now(),
-  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now()
+  updated_at timestamp NOT NULL DEFAULT now() ON UPDATE now(),
+
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (thread_id) REFERENCES threads (id)
 );
 
 CREATE TABLE thread_messages (
-  id int AUTO_INCREMENT PRIMARY KEY,
-  thread_id int NOT NULL REFERENCES threads (id),
-  sender_id int NOT NULL REFERENCES users (id),
+  id int unsigned AUTO_INCREMENT PRIMARY KEY,
+  thread_id int unsigned NOT NULL REFERENCES threads (id),
+  sender_id int unsigned NOT NULL REFERENCES users (id),
   message_type varchar(4) NOT NULL DEFAULT 'TEXT' CHECK (message_type in ('TEXT')),
   -- pubnub timestamp.
   message text NOT NULL,
-  timestamp bigint NOT NULL
+  timestamp bigint unsigned NOT NULL,
+
+  FOREIGN KEY (thread_id) REFERENCES threads (id),
+  FOREIGN KEY (sender_id) REFERENCES users (id)
 );
