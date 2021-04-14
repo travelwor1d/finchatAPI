7a94978da3569367de25782e7523b9dcd4caa60f
Nick <65750313+nkanders@users.noreply.github.com>
2021-04-14 12:32:45 -0500

    Capitalized error message

--- internal/controller/auth.go
+++ internal/controller/auth.go
@@ -22,17 +22,17 @@ type loginPayload struct {
 func (ctr *Ctr) Login(c *fiber.Ctx) error {
 	var p loginPayload
 	if err := c.BodyParser(&p); err != nil {
-		return httperr.New(codes.Omit, http.StatusBadRequest, "failed to parse body", err).Send(c)
+		return httperr.New(codes.Omit, http.StatusBadRequest, "Failed to parse body", err).Send(c)
 	}
 	creds, err := ctr.store.GetUserCredsByEmail(c.Context(), p.Email)
 	if errors.Is(err, store.ErrNotFound) {
-		return httperr.New(codes.InvalidCredentials, http.StatusNotFound, "the email you entered does not match an account").Send(c)
+		return httperr.New(codes.InvalidCredentials, http.StatusNotFound, "The email you entered does not match an account").Send(c)
 	}
 	if err != nil {
 		return errInternal.SetDetail(err).Send(c)
 	}
 	if !matches([]byte(creds.Hash), []byte(p.Password)) {
-		return httperr.New(codes.InvalidCredentials, http.StatusBadRequest, "the password you entered is incorrect").Send(c)
+		return httperr.New(codes.InvalidCredentials, http.StatusBadRequest, "The password you entered is incorrect").Send(c)
 	}
 	user, err := ctr.store.GetUser(c.Context(), creds.UserID)
 	if err != nil {
@@ -56,7 +56,7 @@ type registerPayload struct {
 func (ctr *Ctr) Register(c *fiber.Ctx) error {
 	var p registerPayload
 	if err := c.BodyParser(&p); err != nil {
-		return httperr.New(codes.Omit, http.StatusBadRequest, "failed to parse body", err).Send(c)
+		return httperr.New(codes.Omit, http.StatusBadRequest, "Failed to parse body", err).Send(c)
 	}
 	v := validate.Struct(p)
 	if !v.Validate() {
@@ -68,20 +68,20 @@ func (ctr *Ctr) Register(c *fiber.Ctx) error {
 	if inviteCode != "" {
 		userType = "GOAT"
 		if len(inviteCode) != 6 {
-			return httperr.New(codes.Omit, http.StatusBadRequest, "invite code is 6 chars long string").Send(c)
+			return httperr.New(codes.Omit, http.StatusBadRequest, "Invite code is 6 chars long string").Send(c)
 		}
 		status, found, err := ctr.store.GetInviteCodeStatus(c.Context(), inviteCode)
 		if err != nil {
 			return errInternal.SetDetail(err).Send(c)
 		}
 		if !found {
-			return httperr.New(codes.Omit, http.StatusBadRequest, "invalid invite code").Send(c)
+			return httperr.New(codes.Omit, http.StatusBadRequest, "Invalid invite code").Send(c)
 		}
 		if status == "USED" {
-			return httperr.New(codes.Omit, http.StatusBadRequest, "invite code was already used").Send(c)
+			return httperr.New(codes.Omit, http.StatusBadRequest, "Invite code was already used").Send(c)
 		}
 		if status == "EXPIRED" {
-			return httperr.New(codes.Omit, http.StatusBadRequest, "invite code expired")
+			return httperr.New(codes.Omit, http.StatusBadRequest, "Invite code expired")
 		}
 	} else {
 		userType = "USER"
@@ -93,7 +93,7 @@ func (ctr *Ctr) Register(c *fiber.Ctx) error {
 		return httperr.New(
 			codes.EmailAlreadyTaken,
 			http.StatusBadRequest,
-			"user with provided email already exists",
+			"User with provided email already exists",
 		).Send(c)
 	}
 	if err != nil {
