426e96a37808345fb98deefb8c6483015833092e
travelwor1d <travel@git.com>
2021-04-18 17:22:21 +0200

    add comments

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -22,29 +22,28 @@ import (
 )
 
 func main() {
+	// Initialize configuration service.
 	appconfig.Init("configs/config.yaml")
 
+	// Configure Twilio client.
 	c := twilio.NewClient(appconfig.Config.Twilio.SID, appconfig.Config.Twilio.Token, nil).Verify.Verifications
 	verifySvc := verify.New(c, appconfig.Config.Twilio.Verify)
 
+	// Configure Stripe client.
 	stripe.Key = appconfig.Config.Stripe.Key
 
+	// Configure Firebase Authenticatio client.
 	firebaseapp, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
 		log.Fatalf("failed initialize firebase app: %v", err)
 	}
-
 	auth, err := firebaseapp.Auth(context.Background())
 	if err != nil {
 		log.Fatalf("failed initialize firebase auth client: %v", err)
 	}
 	_ = auth
 
-	db, err := store.Connect(appconfig.Config.MySQL)
-	if err != nil {
-		log.Fatalf("failed to connect to mySQL db: %v", err)
-	}
-
+	// Configure Cloud Storage and upload service.
 	storageClint, err := storage.NewClient(context.Background())
 	if err != nil {
 		log.Fatalf("failed to create storage service: %v", err)
@@ -52,15 +51,24 @@ func main() {
 	bkt := storageClint.Bucket(appconfig.Config.Storage.BucketName)
 	u := upload.New(bkt)
 
+	// Connect to mySQL database and configure store service.
+	db, err := store.Connect(appconfig.Config.MySQL)
+	if err != nil {
+		log.Fatalf("failed to connect to mySQL db: %v", err)
+	}
 	s := store.New(db)
-	jwtM := token.NewJWTManager(appconfig.Config.Auth.Secret, time.Duration(appconfig.Config.Auth.Duration)*time.Minute)
+
+	// Configure messaging service.
 	msg := messaging.New(appconfig.Config.Pubnub, s)
+
+	jwtM := token.NewJWTManager(appconfig.Config.Auth.Secret, time.Duration(appconfig.Config.Auth.Duration)*time.Minute)
+
+	// Setup application controller with its dependencies.
 	ctr := controller.New(s, jwtM, verifySvc, u, msg)
 
+	// Configure and run fiber.
 	a := fiber.New()
-
 	app.Setup(a, ctr)
-
 	addr := fmt.Sprintf(":%s", appconfig.Config.Port)
 	log.Fatal(a.Listen(addr))
 }
