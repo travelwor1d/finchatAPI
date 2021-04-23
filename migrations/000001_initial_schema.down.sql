d8c737f49670a0b78127abbb78f292a7b6112824
travelwor1d <travel@git.com>
2021-04-23 11:53:29 +0200

    update schema down migrations

--- migrations/000001_initial_schema.down.sql
+++ migrations/000001_initial_schema.down.sql
@@ -1,2 +1,8 @@
 DROP VIEW posts_with_votes;
-DROP TABLE post_votes, posts, comments, goat_invite_codes, users;
+DROP VIEW contact_requests, contacts;
+
+DROP TABLE thread_messages, thread_participants; -- Probably tables are not needed.
+DROP TABLE threads; -- Probably table is not needed.
+DROP TABLE comments, post_votes;
+DROP TABLE posts, goat_invite_codes, users_contacts;
+DROP TABLE users;
